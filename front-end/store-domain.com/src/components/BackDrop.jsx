e20ff254474a1681e2c0eda3c6c753ad5215a6dd
Oustaa <otailaba98@gmail.com>
2025-06-22 13:58:29 +0100

    deleting the old react-app based project

--- front-end/store-domain.com/src/components/BackDrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledBackDrop } from "../styles";
-
-const BackDrop = ({ clickHandler, dark }) => {
-  return <StyledBackDrop dark={dark} onClick={clickHandler} />;
-};
-
-export default BackDrop;
